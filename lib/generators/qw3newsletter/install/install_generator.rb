585601cc606ff2595ce9b720604c0ab42661d848
QW3 <contato@qw3.com.br>
2012-04-13 14:00:43 -0300

    .

--- lib/generators/qw3newsletter/install/install_generator.rb
+++ lib/generators/qw3newsletter/install/install_generator.rb
@@ -15,7 +15,7 @@ module Qw3newsletter
       # taken from http://github.com/rails/rails/blob/master/activerecord/lib/generators/active_record.rb
       def self.next_migration_number(dirname)
         if ActiveRecord::Base.timestamped_migrations
-          Time.now.utc.strftime("%Y%m%d%H%M%S").to_s + rand(1230).to_s
+          Time.now.utc.strftime("%Y%m%d%H%M%S").to_s
         else
           "%.3d" % (current_migration_number(dirname) + 1)
         end
@@ -24,6 +24,7 @@ module Qw3newsletter
       def create_migrations
         if yes? 'Criar migrations?'
           migration_template 'db/migrate/create_newsletters.rb', 'db/migrate/create_newsletters.rb'
+          sleep( 1.0 ) # Espera 1 segundo para que o timestamp da proxima migration seja diferente
           rake 'db:migrate'
         end
       end
@@ -44,17 +45,21 @@ module Qw3newsletter
       end
       
       def insert_menu_calls
-        append_file "app/views/administrator/administradores/_menu_detalhes.html.erb" do
-          "<li>
+        if yes?( "Inserir chamadas de menu?" )
+          append_file "app/views/administrator/administradores/_menu_detalhes.html.erb" do
+            "<li>
   <%= link_to '/administrator/newsletters/index', :class => \"newsletters-leftbar\" do %>
     <span>Cadastros no Newsletter</span>
   <% end %>
 </li>\n"
+          end
         end
       end
       
       def create_files
-        copy_file 'public/javascripts/qw3newsletters.js', 'public/javascripts/qw3/newsletters.js'
+        if yes?( "Copiar arquivos?" )
+          copy_file 'public/javascripts/qw3newsletters.js', 'public/javascripts/qw3/newsletters.js'
+        end
       end
       
     end
--- lib/qw3newsletter/version.rb
+++ lib/qw3newsletter/version.rb
@@ -1,3 +1,3 @@
 module Qw3newsletter
-  VERSION = "0.1.2"
+  VERSION = "0.1.3"
 end
